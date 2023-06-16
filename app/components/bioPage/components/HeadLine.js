26265626e811ac7127e91d92b2ea95d347611f5c
Karlos <karin_ar72@hotmail.com>
2023-06-16 15:24:31 -0600

    fix: translate the bio's headline

--- app/components/bioPage/components/HeadLine.js
+++ app/components/bioPage/components/HeadLine.js
@@ -1,19 +1,31 @@
+"use client";
+// css
+import styles from "./styles/HeadLine.module.scss";
+// system tools
 import React from "react";
 import Photo from "./photo";
-import styles from "./styles/HeadLine.module.scss";
+import { usePathname } from "next/navigation";
 
 export default function HeadLine() {
+  const url = usePathname();
+  const isSpanish = url.includes("spanish");
   return (
     <section className={styles.headlineBox}>
       <Photo />
       <div className={styles.textBox}>
         <h1>Carlos Karin Arroyo Chagoya</h1>
-        <p>
-          I&apos;m a Frontend Delevoper. I work to make products that people
-          love by having a good comunication with the end user to constantly
-          make improvements on the product. I like to help boost a great culture
-          in the teams.
-        </p>
+        {isSpanish ? (
+          <p>
+            Trabajo para construir aplicaciones grandiosas y convertirme en
+            desarrollador Full-Stack. Estoy en busca de un equipo con quienes
+            crecer juntos.
+          </p>
+        ) : (
+          <p>
+            Aiming to build great web apps and become a Full-Stack developer. I
+            want to find a great team with whom grow along.
+          </p>
+        )}
       </div>
     </section>
   );
