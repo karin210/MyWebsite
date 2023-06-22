34a1ddc87d40d0da85af715c7a36c9e98573994a
Karlos <karin_ar72@hotmail.com>
2023-06-21 23:48:09 -0600

    style: set language selected appearence like button with blue text

--- app/components/footer/contact/Contact.js
+++ app/components/footer/contact/Contact.js
@@ -5,11 +5,11 @@ import React from "react";
 // statics
 import Icon from "./components/Icon";
 import LinkedInIcon from "../../../../public/linkedin-svgrepo-com.svg";
-import FacebookIcon from "../../../../public/facebook-svgrepo-com.svg";
+
 import GitHubIcon from "../../../../public/github-svgrepo-com.svg";
-import WhatsAppIcon from "../../../../public/whatsapp-whats-app-svgrepo-com.svg";
+
 import EmailIcon from "../../../../public/email-address-svgrepo-com.svg";
-import InstagramIcon from "../../../../public/instagram-1-svgrepo-com.svg";
+
 import TitterIcon from "../../../../public/twitter-color-svgrepo-com.svg";
 import Image from "next/image";
 
@@ -17,14 +17,6 @@ export default function Contact() {
   return (
     <section className={styles.iconsBox}>
       <div className={styles.noLinkIcons}>
-        {/* <div>
-          <Image
-            className={styles.img}
-            src={WhatsAppIcon}
-            alt="WhatsApp icon"
-          />
-          <span>332-252-9630</span>
-        </div> */}
         <div>
           <Image className={styles.img} src={EmailIcon} alt="Email icon" />
           <span>karin_ar72@hotmail.com</span>
@@ -35,16 +27,7 @@ export default function Contact() {
         src={LinkedInIcon}
         alt="LinkedIn icon"
       />
-      {/* <Icon
-        link="https://www.facebook.com/profile.php?id=100009669095788"
-        src={FacebookIcon}
-        alt="Facebook icon"
-      />
-      <Icon
-        link="https://www.instagram.com/karin_arroyo10/"
-        src={InstagramIcon}
-        alt="Instagram icon"
-      /> */}
+
       <Icon
         link="https://github.com/karin210"
         src={GitHubIcon}
